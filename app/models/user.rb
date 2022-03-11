9245342f95b15531d4115dc6802456afbc51d9b1
Adrian Menares <adrianmenares22@gmail.com>
2022-03-11 13:34:56 -0300

    fix: corregir labels de form Products

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :phone, numericality: { only_integer: true}, presence: true
+  validates :phone, numericality: { only_integer: true}, length: {minimum:9, maximum:9}, presence: true
 end
