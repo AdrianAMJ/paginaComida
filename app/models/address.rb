d9c8c5e5cde260cd7f22ff33790187124882075f
Adrian Menares <adrianmenares22@gmail.com>
2022-03-14 00:23:14 -0300

    feat: añadir traducciones y validaciones a Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,15 @@
 class Address < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
+
+    validates :direccion, presence: true
+    validates :comuna, presence: true
+
+    enum comuna: {
+        las_condes: 'Las Condes',
+        la_reina: 'La Reina',
+        providencia: 'Providencia',
+        vitacura: 'Vitacura',
+        nunoa: 'Ñuñoa',
+    }
+  
 end
