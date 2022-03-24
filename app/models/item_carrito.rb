dc1e8070945ed6aab31016a1288cdbff9eaf7ad5
Adrian Menares <adrianmenares22@gmail.com>
2022-03-24 13:19:53 -0300

    feat: añadir mensaje de carrito vacio en index

--- app/models/item_carrito.rb
+++ app/models/item_carrito.rb
@@ -1,4 +1,7 @@
 class ItemCarrito < ApplicationRecord
     belongs_to :carrito, foreign_key: :carrito_id
     belongs_to :product, foreign_key: :product_id
+
+    validates :carrito, presence: true
+    validates :product, presence: true
 end
